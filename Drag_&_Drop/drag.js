99fe8913be4643db94b4b768114c2dcb31623405
mahesh9561 <maheshpathak200@gmail.com>
2023-09-07 17:03:45 +0530

    Drag_&_Drop

--- Drag_&_Drop/drag.js
+++ Drag_&_Drop/drag.js
@@ -1,6 +1,32 @@
 const dragables = document.querySelectorAll('.task');
 const dropables = document.querySelectorAll('.swim-lane');
 
+const inputText = document.querySelector('#todo-input');
+const button = document.querySelector('#button')
+
+button.addEventListener('click', () => {
+    const textInput = inputText.value.trim(); // Use "value" to get the input value, not "ariaValueMax"
+
+    if (textInput === '') {
+        alert("Please enter a value.");
+    } else {
+        // Create a new task element and set its innerHTML
+        const newTask = document.createElement('div');
+        newTask.classList.add('task');
+        newTask.innerHTML = `<span>${textInput}</span>`;
+
+        // Append the new task to a swim-lane (you need to decide which swim-lane to append it to)
+        // For now, I'm assuming you want to append it to the first swim-lane:
+        const firstSwimLane = dropables[0];
+        firstSwimLane.appendChild(newTask);
+
+        console.log(textInput);
+
+        // Clear the input field after adding a task
+        inputText.value = '';
+    }
+});
+
 dragables.forEach(dragable => {
     dragable.addEventListener('dragstart', () => {
         console.log('dragable')
